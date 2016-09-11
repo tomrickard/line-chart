b14689bc5ddb2781a7e37fd14c9071582d94bd01
Thomas Rickard <thomas_rickard@yahoo.co.uk>
2016-09-11 10:35:42 +0100

    update not fixed

--- dist/line-chart.js
+++ dist/line-chart.js
@@ -45,6 +45,7 @@ var lineChart =
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
+	"use strict";
 	var d3 = __webpack_require__(1);
 	//- Implementation -------------------------------------------
 	function chart(selection, name) {
